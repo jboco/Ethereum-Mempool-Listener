34bb637dc97d4d3a5011ad3e1dc703630f438304
jboco <jboco@terpmail.umd.edu>
2023-08-17 22:49:01 -0600

    Update listener.ts

--- listener.ts
+++ listener.ts
@@ -1,11 +1,7 @@
-//import {ethers} from "ethers"
-const ethers = require("ethers")
-import {config} from "dotenv"
-config ();
-
+const ethers = require("ethers");
 
 async function mempoolListener() {
-    const WsProvider = new ethers.providers.WebSocketProvider('wss://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}');
+    const WsProvider = new ethers.providers.WebSocketProvider("ADD_YOUR_ETHEREUM_NODE_WSS_URL"); 
     WsProvider.on("pending", async (tx) => {
     const  txInfo = await WsProvider.getTransaction(tx);
     console.log(txInfo);
